424840446aa5af7f5a00c2a6132194763e42d81d
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2026-01-18 10:24:51 +0800

    readable cleanup

--- main.c
+++ main.c
@@ -241,180 +241,215 @@ void system_startup(void)
     lcd_startup_screen();
 }
 
+void auto_feed_task(void)
+{
+    unsigned char i;
+
+    if (!is_time_set || !update_lcd)
+        return;
+
+    update_lcd = 0;
+    lcd_show_time();
+
+    if (ssec != 0)
+        return;
+
+    for (i = 0; i < total_jadual; i++)
+    {
+        if (jadwal[i].a &&
+            hour == jadwal[i].h &&
+            mmin == jadwal[i].m)
+        {
+            lcd_cmd(0xC0);
+            lcd_str(" AUTO: MAKAN!    ");
+
+            servo_buka();
+            servo_tutup();
+
+            uart_puts("Feed Done\r\n");
+
+            lcd_cmd(0xC0);
+            lcd_str(" AUTO: SELESAI   ");
+        }
+    }
+}
+
+void bt_set_initial_time(char c)
+{
+    if (c == 't' || c == 'T')
+    {
+        rx_index = 0;
+        rx_buffer[rx_index++] = c;
+        return;
+    }
+
+    if (rx_index == 0)
+        return;
+
+    rx_buffer[rx_index++] = c;
+
+    if (rx_index == 7)
+    {
+        hour = (rx_buffer[1] - '0') * 10 + (rx_buffer[2] - '0');
+        mmin = (rx_buffer[3] - '0') * 10 + (rx_buffer[4] - '0');
+        ssec = (rx_buffer[5] - '0') * 10 + (rx_buffer[6] - '0');
+
+        is_time_set = 1;
+
+        lcd_cmd(0x01);
+        lcd_cmd(0x80);
+        lcd_str("TIME UPDATED!");
+        delay_ms(1000);
+        lcd_cmd(0x01);
+
+        rx_index = 0;
+    }
+}
+
+void bt_manual_feed(void)
+{
+    lcd_cmd(0xC0);
+    lcd_str("  MANUAL FEED...  ");
+
+    servo_buka();
+    servo_tutup();
+
+    uart_puts("Feed Done\r\n");
+
+    lcd_cmd(0xC0);
+    lcd_str("  STATUS: READY   ");
+}
+
+void bt_command_task(char c)
+{
+    unsigned char i;
+    char str_time[6];
+
+    /* Manual feed */
+    if (c == 'm' || c == 'M')
+    {
+        bt_manual_feed();
+        return;
+    }
+
+    /* Show status */
+    if (c == 's' || c == 'S')
+    {
+        uart_puts("Active Schedule: ");
+        uart_write(total_jadual + '0');
+        uart_puts("\r\n");
+
+        for (i = 0; i < total_jadual; i++)
+        {
+            uart_write(i + 1 + '0');
+            uart_puts(" [");
+
+            str_time[0] = (jadwal[i].h / 10) + '0';
+            str_time[1] = (jadwal[i].h % 10) + '0';
+            str_time[2] = ':';
+            str_time[3] = (jadwal[i].m / 10) + '0';
+            str_time[4] = (jadwal[i].m % 10) + '0';
+            str_time[5] = '\0';
+
+            uart_puts(str_time);
+            uart_puts("] - ");
+
+            if (hour > jadwal[i].h ||
+                (hour == jadwal[i].h && mmin >= jadwal[i].m))
+                uart_puts("Done\r\n");
+            else
+                uart_puts("Not Yet\r\n");
+        }
+        return;
+    }
+
+    /* Command start */
+    if (c == 't' || c == 'T' ||
+        c == 'f' || c == 'F' ||
+        c == 'c' || c == 'C')
+    {
+        rx_index = 0;
+        rx_buffer[rx_index++] = c;
+        return;
+    }
+
+    if (rx_index == 0)
+        return;
+
+    rx_buffer[rx_index++] = c;
+
+    /* Set time */
+    if ((rx_buffer[0] == 't' || rx_buffer[0] == 'T') && rx_index == 7)
+    {
+        hour = (rx_buffer[1] - '0') * 10 + (rx_buffer[2] - '0');
+        mmin = (rx_buffer[3] - '0') * 10 + (rx_buffer[4] - '0');
+        ssec = (rx_buffer[5] - '0') * 10 + (rx_buffer[6] - '0');
+        uart_puts("TIME OK\r\n");
+        rx_index = 0;
+    }
+
+    /* Set schedule */
+    else if ((rx_buffer[0] == 'f' || rx_buffer[0] == 'F') && rx_index == 6)
+    {
+        unsigned char n = rx_buffer[1] - '0';
+
+        if (n >= 1 && n <= MAX_JADUAL)
+        {
+            jadwal[n - 1].h =
+                (rx_buffer[2] - '0') * 10 + (rx_buffer[3] - '0');
+            jadwal[n - 1].m =
+                (rx_buffer[4] - '0') * 10 + (rx_buffer[5] - '0');
+            jadwal[n - 1].a = 1;
+
+            lcd_cmd(0xC0);
+            lcd_str("  SCHEDULE SET!   ");
+            delay_ms(1000);
+            lcd_cmd(0xC0);
+            lcd_str("                  ");
+        }
+        rx_index = 0;
+    }
+
+    /* Set count */
+    else if ((rx_buffer[0] == 'c' || rx_buffer[0] == 'C') && rx_index == 2)
+    {
+        unsigned char new_count = rx_buffer[1] - '0';
+
+        if (new_count >= 1 && new_count <= MAX_JADUAL)
+        {
+            total_jadual = new_count;
+            uart_puts("Total Schedule: ");
+            uart_write(total_jadual + '0');
+            uart_puts("\r\n");
+        }
+        rx_index = 0;
+    }
+}
+
+
+
+
 void main(void)
 {
     unsigned char i;
     char temp_char;
     char str_time[6];
+    char rx;
 
     system_startup();
 
     while (1)
     {
-
-        // --- LOGIK AUTO FEED ---
-        if (is_time_set == 1 && update_lcd)
-        {
-            update_lcd = 0;
-            lcd_show_time();
-
-            if (ssec == 0)
-            {
-                for (i = 0; i < total_jadual; i++)
-                {
-                    if ((hour == jadwal[i].h && mmin == jadwal[i].m) && jadwal[i].a == 1)
-                    {
-                        lcd_cmd(0xC0);
-                        lcd_str(" AUTO: MAKAN!    ");
-                        servo_buka();
-                        servo_tutup();
-                        uart_puts("Feed Done\r\n");
-                        lcd_cmd(0xC0);
-                        lcd_str(" AUTO: SELESAI   ");
-                    }
-                }
-            }
-        }
+        auto_feed_task();
 
         if (RI)
         {
-            temp_char = uart_read();
+            rx = uart_read();
 
-            if (is_time_set == 0)
-            {
-                if (temp_char == 't' || temp_char == 'T')
-                {
-                    rx_index = 0;
-                    rx_buffer[rx_index++] = temp_char;
-                }
-                else if (rx_index > 0)
-                {
-                    rx_buffer[rx_index++] = temp_char;
-                    if (rx_index == 7)
-                    {
-                        hour = (rx_buffer[1] - '0') * 10 + (rx_buffer[2] - '0');
-                        mmin = (rx_buffer[3] - '0') * 10 + (rx_buffer[4] - '0');
-                        ssec = (rx_buffer[5] - '0') * 10 + (rx_buffer[6] - '0');
-                        is_time_set = 1;
-                        lcd_cmd(0x01);
-                        lcd_cmd(0x80);
-                        lcd_str("TIME UPDATED!");
-                        delay_ms(1000);
-                        lcd_cmd(0x01);
-                        rx_index = 0;
-                    }
-                }
-            }
+            if (!is_time_set)
+                bt_set_initial_time(rx);
             else
-            {
-                if (temp_char == 'm' || temp_char == 'M')
-                {
-                    lcd_cmd(0xC0);
-                    lcd_str("  MANUAL FEED...  ");
-                    servo_buka();
-                    servo_tutup();
-                    uart_puts("Feed Done\r\n");
-                    lcd_cmd(0xC0);
-                    lcd_str("  STATUS: READY   ");
-                }
-
-                // --- STATUS PAPARAN FLEXIBLE ---
-                else if (temp_char == 's' || temp_char == 'S')
-                {
-                    uart_puts("Active Schedule: ");
-                    uart_write(total_jadual + '0'); // Error fixed here
-                    uart_puts("\r\n");
-
-                    for (i = 0; i < total_jadual; i++)
-                    {
-                        uart_write(i + 1 + '0'); // Error fixed here
-                        uart_puts(" [");
-
-                        str_time[0] = (jadwal[i].h / 10) + '0';
-                        str_time[1] = (jadwal[i].h % 10) + '0';
-                        str_time[2] = ':';
-                        str_time[3] = (jadwal[i].m / 10) + '0';
-                        str_time[4] = (jadwal[i].m % 10) + '0';
-                        str_time[5] = '\0';
-                        uart_puts(str_time);
-
-                        uart_puts("] - ");
-
-                        if (hour > jadwal[i].h || (hour == jadwal[i].h && mmin >= jadwal[i].m))
-                            uart_puts("Done\r\n");
-                        else
-                            uart_puts("Not Yet\r\n");
-                    }
-                }
-
-                // --- DETECT COMMAND ---
-                else if (temp_char == 't' || temp_char == 'T' ||
-                         temp_char == 'f' || temp_char == 'F' ||
-                         temp_char == 'c' || temp_char == 'C')
-                {
-                    rx_index = 0;
-                    rx_buffer[rx_index++] = temp_char;
-                }
-
-                else if (rx_index > 0)
-                {
-                    rx_buffer[rx_index++] = temp_char;
-
-                    // --- SET TIME ---
-                    if ((rx_buffer[0] == 't' || rx_buffer[0] == 'T') && rx_index == 7)
-                    {
-                        hour = (rx_buffer[1] - '0') * 10 + (rx_buffer[2] - '0');
-                        mmin = (rx_buffer[3] - '0') * 10 + (rx_buffer[4] - '0');
-                        ssec = (rx_buffer[5] - '0') * 10 + (rx_buffer[6] - '0');
-                        uart_puts("TIME OK\r\n");
-                        rx_index = 0;
-                    }
-
-                    // --- SET JADUAL ---
-                    else if ((rx_buffer[0] == 'f' || rx_buffer[0] == 'F') && rx_index == 6)
-                    {
-                        unsigned char alarm_num = rx_buffer[1] - '0';
-
-                        if (alarm_num >= 1 && alarm_num <= MAX_JADUAL)
-                        {
-                            unsigned char h = (rx_buffer[2] - '0') * 10 + (rx_buffer[3] - '0');
-                            unsigned char m = (rx_buffer[4] - '0') * 10 + (rx_buffer[5] - '0');
-
-                            jadwal[alarm_num - 1].h = h;
-                            jadwal[alarm_num - 1].m = m;
-
-                            lcd_cmd(0xC0);
-                            lcd_str("  SCHEDULE SET!   ");
-                            delay_ms(1000);
-                            lcd_cmd(0xC0);
-                            lcd_str("                  ");
-                        }
-                        rx_index = 0;
-                    }
-
-                    // --- UBAH JUMLAH JADUAL (CN) ---
-                    else if ((rx_buffer[0] == 'c' || rx_buffer[0] == 'C') && rx_index == 2)
-                    {
-                        unsigned char new_count = rx_buffer[1] - '0';
-
-                        if (new_count >= 1 && new_count <= MAX_JADUAL)
-                        {
-                            total_jadual = new_count;
-                            uart_puts("Total Schedule: ");
-                            uart_write(total_jadual + '0'); // Error fixed here
-                            uart_puts("\r\n");
-
-                            lcd_cmd(0xC0);
-                            lcd_str("  SCHD COUNT:   ");
-                            lcd_write(total_jadual + '0');
-                            delay_ms(1000);
-                            lcd_cmd(0xC0);
-                            lcd_str("                  ");
-                        }
-                        rx_index = 0;
-                    }
-                }
-            }
+                bt_command_task(rx);
         }
     }
 }
